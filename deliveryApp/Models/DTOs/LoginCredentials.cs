b332f58e598b567215ecb291c5c2a7bae859650e
2slw4u <mr.fyed@mail.ru>
2023-10-18 18:24:42 +0700

    Added LoginCredentials class

---
+++ deliveryApp/Models/DTOs/LoginCredentials.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.DTOs
+{
+    public class LoginCredentials
+    {
+        [Required]
+        [MinLength(1)]
+        public string email { get; set; }
+        [Required]
+        [MinLength(1)]
+        public string password { get; set; }
+    }
+}
