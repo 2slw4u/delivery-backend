91f895a8816d74d3596d7ac111ad1ce9d65486b1
2slw4u <mr.fyed@mail.ru>
2023-10-18 18:18:26 +0700

    Added DishBasketDto class

---
+++ deliveryApp/Models/DTOs/DishBasketDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace deliveryApp.Models.DTOs
+{
+    public class DishBasketDto
+    {
+        public Guid Id { get; set; }
+        [Required]
+        [MinLength(1)]
+        public string name { get; set; }
+        [Required]
+        public double price { get; set; }
+        [Required]
+        public double totalPrice { get; set; }
+        [Required]
+        public int amount { get; set; }
+        public string? image { get; set; }
+    }
+}
