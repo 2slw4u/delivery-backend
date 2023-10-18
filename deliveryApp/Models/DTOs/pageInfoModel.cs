f067cfeb4fb903fd74d5d5609ecac4fa9f5215ea
2slw4u <mr.fyed@mail.ru>
2023-10-18 18:36:29 +0700

    Added PageInfoModel class

---
+++ deliveryApp/Models/DTOs/pageInfoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace deliveryApp.Models.DTOs
+{
+    public class pageInfoModel
+    {
+        public int size { get; set; }
+        public int count { get; set; }
+        public int current { get; set; }
+    }
+}
