a164f79e4b9b1320766b9dc88340664ba63b4e29
2slw4u <mr.fyed@mail.ru>
2023-10-18 22:46:39 +0700

    Added OrderEntity class

---
+++ deliveryApp/Models/Entities/OrderEntity.cs
@@ -0,0 +1,23 @@
+﻿using deliveryApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.Entities
+{
+    public class OrderEntity
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public DateTime DeliveryTime { get; set; }
+        [Required]
+        public DateTime OrderTime { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [Required]
+        public string AddresId { get; set; }
+        [Required]
+        public OrderStatus Status { get; set; }
+        [Required]
+        public UserEntity User { get; set; }
+
+    }
+}
