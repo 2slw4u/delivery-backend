bcfe4113c8b22850d16efac565ded0e4c3f3230b
2slw4u <mr.fyed@mail.ru>
2023-10-18 22:33:43 +0700

    Added DishEntity class

---
+++ deliveryApp/Models/Entities/DishEntity.cs
@@ -0,0 +1,21 @@
+﻿using deliveryApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.Entities
+{
+    public class DishEntity
+    {
+        public Guid id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public double Price { get; set; }
+        public string? Description { get; set; }
+        [Required]
+        public Boolean IsVegetarian { get; set; }
+        public string? Photo { get; set; }
+        [Required]
+        public DishCategory Category { get; set; }
+        public List<RatingEntity> Ratings { get; set; }
+    }
+}
