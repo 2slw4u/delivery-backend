04a00a6cf3f74731ccc91e1b8067684e5b155794
2slw4u <mr.fyed@mail.ru>
2023-10-18 22:42:53 +0700

    Added UserEntity class

---
+++ deliveryApp/Models/Entities/UserEntity.cs
@@ -0,0 +1,23 @@
+﻿using deliveryApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        [Required]
+        public Gender Gender { get; set; }
+        [Phone]
+        public string? Phone { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string? Address { get; set; }
+        public List<RatingEntity>? Ratings { get; set; }
+        public List<OrderEntity>? Orders { get; set; }
+    }
+}
