49b314d42549f0cba47760905557b9dfd0c47947
2slw4u <mr.fyed@mail.ru>
2023-10-18 18:38:00 +0700

    Added Response class

---
+++ deliveryApp/Models/DTOs/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace deliveryApp.Models.DTOs
+{
+    public class Response
+    {
+        public string? status;
+        public string? message;
+    }
+}
--- deliveryApp/Models/DTOs/pageInfoModel.cs
+++ deliveryApp/Models/DTOs/pageInfoModel.cs
@@ -2,5 +2,8 @@
 {
     public class PageInfoModel
     {
+        public int size { get; set; }
+        public int count { get; set; }
+        public int current { get; set; }
     }
 }
