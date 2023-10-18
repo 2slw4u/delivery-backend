aadf47608e931f61cb2c8baf4792b43d019826eb
2slw4u <mr.fyed@mail.ru>
2023-10-18 20:22:58 +0700

    Added SearchAddressModel class

---
+++ deliveryApp/Models/DTOs/SearchAddressModel.cs
@@ -0,0 +1,13 @@
+﻿using deliveryApp.Models.Enums;
+
+namespace deliveryApp.Models.DTOs
+{
+    public class SearchAddressModel
+    {
+        public int objectId { get; set; }
+        public Guid objectGuid { get; set; }
+        public string? text { get; set; }
+        public GarAddressLevel objectLevel { get; set; }
+        public string? objectLevelText { get; set; }
+    }
+}
