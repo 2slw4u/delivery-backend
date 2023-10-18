74b13e6ee2dcdd47239dc598eb76e1d995a99f94
2slw4u <mr.fyed@mail.ru>
2023-10-18 18:33:55 +0700

    Added OrderInfoDto class

---
+++ deliveryApp/Models/DTOs/OrderInfoDto.cs
@@ -0,0 +1,18 @@
+﻿using deliveryApp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.DTOs
+{
+    public class OrderInfoDto
+    {
+        public Guid id { get; set; }
+        [Required]
+        public DateTime deliveryTime { get; set; }
+        [Required]
+        public DateTime orderTime { get; set; }
+        [Required]
+        public OrderStatus status { get; set; }
+        [Required]
+        public double price { get; set; }
+    }
+}
