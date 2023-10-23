b4c1660f83f27b1bae0d4ee114945b5e6456bd20
2slw4u <mr.fyed@mail.ru>
2023-10-23 22:20:12 +0700

    Added AddressElementEntity class (without atribute specifications)

--- deliveryApp/Models/Entities/HierarchyEntity.cs
+++ deliveryApp/Models/Entities/HierarchyEntity.cs
@@ -6,5 +6,7 @@
         public int ObjectId { get; set; }
         public int ParentObjId { get; set; }
         public Boolean IsActive { get; set; }
+        public HouseEntity? House { get; set; }
+        public AddressElementEntity? AddressElement { get; set; }
     }
 }
