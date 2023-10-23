9b1ba5e43e112058bd1b84876d509b6b83431448
2slw4u <mr.fyed@mail.ru>
2023-10-23 21:57:28 +0700

    Added TokenEntity class

---
+++ deliveryApp/Models/Entities/TokenEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.Entities
+{
+    public class TokenEntity
+    {
+        public Guid id;
+        [Required]
+        public string token { get; set; }
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+    }
+}
