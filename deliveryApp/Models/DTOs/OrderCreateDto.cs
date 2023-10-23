d978afbeadfcaf997b546c976ba7f9be8909b838
2slw4u <mr.fyed@mail.ru>
2023-10-23 22:10:59 +0700

    PascalCase'd the hell out of the classes

--- deliveryApp/Models/DTOs/OrderCreateDto.cs
+++ deliveryApp/Models/DTOs/OrderCreateDto.cs
@@ -1,6 +1,12 @@
-﻿namespace deliveryApp.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace deliveryApp.Models.DTOs
 {
     public class OrderCreateDto
     {
+        [Required]
+        public DateTime DeliveryTime { get; set; }
+        [Required]
+        public Guid AddressId { get; set; }
     }
 }
