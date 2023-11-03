1dce16f8418a25d25936a12c71dec14382aff6c2
2slw4u <mr.fyed@mail.ru>
2023-11-03 18:15:03 +0700

    Added password to UserEnity; Created migration

--- deliveryApp/Controllers/UserController.cs
+++ deliveryApp/Controllers/UserController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using deliveryApp.Models.DTOs;
+using deliveryApp.Models.Entities;
+using deliveryApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace deliveryApp.Controllers
 {
@@ -7,6 +10,16 @@ namespace deliveryApp.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
 
+        [HttpPost]
+        [Route("register")]
+        public async Task<TokenResponse> Register([FromBody] UserRegisterModel newUser)
+        {
+            return await _userService.Register(newUser);
+        }
     }
 }
