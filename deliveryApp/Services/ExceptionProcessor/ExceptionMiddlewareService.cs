7d84a1e28fae7508ebca599e38c3b926ec68417a
2slw4u <mr.fyed@mail.ru>
2023-11-04 00:28:27 +0700

    Task 21243 (add middleware) done

---
+++ deliveryApp/Services/ExceptionProcessor/ExceptionMiddlewareService.cs
@@ -0,0 +1,50 @@
+﻿using deliveryApp.Models.DTOs;
+using deliveryApp.Models.Exceptions;
+
+namespace deliveryApp.Services.ExceptionProcessor
+{
+    public class ExceptionMiddlewareService
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddlewareService(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (NotFound ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                await context.Response.WriteAsJsonAsync(ex);
+            }
+            catch (Forbidden ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsJsonAsync(ex);
+            }
+            catch (Conflict ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status409Conflict;
+                await context.Response.WriteAsJsonAsync(ex);
+            }
+            catch (BadRequest ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                await context.Response.WriteAsJsonAsync(ex);
+            }
+            catch (Exception ex)
+            {
+                var result = new Response()
+                {
+                    Status = "500",
+                    Message = ex.Message
+                };
+                await context.Response.WriteAsJsonAsync(result);
+            }
+        }
+    }
+}
---
+++ deliveryApp/Services/ExceptionProcessor/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace deliveryApp.Services.ExceptionProcessor
+{
+    public static class MiddlewareExtensions
+    {
+        public static void UseExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddlewareService>();
+        }
+    }
+}
