00ddf7fd4a4d7e893ba59640063e34b56fb3526a
2slw4u <mr.fyed@mail.ru>
2023-11-05 22:26:09 +0700

    Task 18978(get users basket) done

---
+++ deliveryApp/Controllers/BasketController.cs
@@ -0,0 +1,22 @@
+﻿using deliveryApp.Models.DTOs;
+using deliveryApp.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace deliveryApp.Controllers
+{
+    [ApiController]
+    [Route("api/basket")]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketService _basketService;
+        public BasketController (IBasketService basketService)
+        {
+            _basketService = basketService;
+        }
+        [HttpGet]
+        public async Task<List<DishBasketDto>> GetBasket(string token)
+        {
+            return await _basketService.Get(token);
+        }
+    }
+}
--- deliveryApp/Services/BasketService.cs
+++ deliveryApp/Services/BasketService.cs
@@ -1,6 +1,46 @@
-﻿namespace deliveryApp.Services
+﻿using deliveryApp.Models;
+using deliveryApp.Models.DTOs;
+using deliveryApp.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace deliveryApp.Services
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
+        private readonly AppDbContext _context;
+
+        public BasketService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task AddDish(string token, Guid dishId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<DishBasketDto>> Get(string token)
+        {
+            var tokenEntity = await _context.Tokens.Where(x => x.Token == token).FirstOrDefaultAsync();
+            var dishes = await _context.DishesInCart.Include(x => x.User).Include(x => x.Dish).Where(x => x.User.Email == tokenEntity.userEmail && x.Order == null).ToListAsync();
+            var result = new List<DishBasketDto>();
+            foreach (var dish in dishes)
+            {
+                DishBasketDto currentDto = new DishBasketDto()
+                {
+                    Id = dish.Dish.Id,
+                    Name = dish.Dish.Name,
+                    Price = dish.Dish.Price,
+                    Amount = dish.Amount,
+                    TotalPrice = dish.Amount * dish.Dish.Price,
+                    Image = dish.Dish.Photo
+                };
+            }
+            return result;
+        }
+
+        public Task RemoveDish(string token, Guid dishId, bool increase = false)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
