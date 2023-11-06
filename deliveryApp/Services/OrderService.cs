1ef0e520bc73c49bc7fbdbfe4f052a68f4da52fd
2slw4u <mr.fyed@mail.ru>
2023-11-06 21:49:39 +0700

    Added try, catch to OrderService

--- deliveryApp/Services/OrderService.cs
+++ deliveryApp/Services/OrderService.cs
@@ -21,92 +21,120 @@ namespace deliveryApp.Services
         {
             await ValidateToken(token);
             await ValidateOrder(token, orderId);
-            var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
-            var orderOfAUserEntity = await _context.Orders.Where(x => x.Id == orderId && x.User.Email == tokenInDB.userEmail).FirstOrDefaultAsync();
-            if (orderOfAUserEntity.Status == OrderStatus.Delievered)
+            try
             {
-                throw new Conflict("Selected order already has confirmed delivery");
+                var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
+                var orderOfAUserEntity = await _context.Orders.Where(x => x.Id == orderId && x.User.Email == tokenInDB.userEmail).FirstOrDefaultAsync();
+                if (orderOfAUserEntity.Status == OrderStatus.Delievered)
+                {
+                    throw new Conflict("Selected order already has confirmed delivery");
+                }
+                orderOfAUserEntity.Status = OrderStatus.Delievered;
+                await _context.SaveChangesAsync();
             }
-            orderOfAUserEntity.Status = OrderStatus.Delievered;
-            await _context.SaveChangesAsync();
+            catch (Exception e)
+            {
+                throw new BadHttpRequestException(e.Message);
+            }
         }
 
         public async Task CreateOrderFromCurrentBasket(string token, OrderCreateDto newOrder)
         {
             await ValidateToken(token);
-            var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
-            var dishesInOrder = await _context.DishesInCart.Where(x => x.User.Email == tokenInDB.userEmail).ToListAsync();
-            if (dishesInOrder.Count == 0)
+            try
             {
-                throw new BadRequest("There is no dishes in users current basket");
+                var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
+                var dishesInOrder = await _context.DishesInCart.Where(x => x.User.Email == tokenInDB.userEmail).ToListAsync();
+                if (dishesInOrder.Count == 0)
+                {
+                    throw new BadRequest("There is no dishes in users current basket");
+                }
+                if ((newOrder.DeliveryTime - DateTime.Now).TotalMinutes < ORDER_AND_DELIVERY_DIFFERENCE)
+                {
+                    throw new Forbidden("Delivery time must be at least an hour after order time");
+                }
+                var totalPrice = 0.0;
+                foreach (var dish in dishesInOrder)
+                {
+                    totalPrice += (dish.Price * dish.Amount);
+                }
+                var result = new OrderEntity()
+                {
+                    Id = Guid.NewGuid(),
+                    DeliveryTime = newOrder.DeliveryTime,
+                    OrderTime = DateTime.Now,
+                    Price = totalPrice,
+                    AddresId = newOrder.AddressId.ToString(),
+                    Status = OrderStatus.InProcess,
+                    User = await _context.Users.Where(x => x.Email == tokenInDB.userEmail).FirstOrDefaultAsync()
+                };
+                await _context.Orders.AddAsync(result);
+                await _context.SaveChangesAsync();
             }
-            if ((newOrder.DeliveryTime - DateTime.Now).TotalMinutes < ORDER_AND_DELIVERY_DIFFERENCE)
+            catch (Exception e)
             {
-                throw new Forbidden("Delivery time must be at least an hour after order time");
+                throw new BadHttpRequestException(e.Message);
             }
-            var totalPrice = 0.0;
-            foreach (var dish in dishesInOrder)
-            {
-                totalPrice += (dish.Price * dish.Amount);
-            }
-            var result = new OrderEntity()
-            {
-                Id = Guid.NewGuid(),
-                DeliveryTime = newOrder.DeliveryTime,
-                OrderTime = DateTime.Now,
-                Price = totalPrice,
-                AddresId = newOrder.AddressId.ToString(),
-                Status = OrderStatus.InProcess,
-                User = await _context.Users.Where(x => x.Email == tokenInDB.userEmail).FirstOrDefaultAsync()
-            };
-            await _context.Orders.AddAsync(result);
-            await _context.SaveChangesAsync();
         }
 
         public async Task<List<OrderDto>> GetAllOrders(string token)
         {
             await ValidateToken(token);
-            var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
-            var allOrders = await _context.Orders.Where(x => x.User.Email == tokenInDB.userEmail).ToListAsync();
-            var result = new List<OrderDto>();
-            foreach (var order in allOrders)
+            try
             {
-                result.Add(await GetOrderInfo(token, order.Id));
+                var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
+                var allOrders = await _context.Orders.Where(x => x.User.Email == tokenInDB.userEmail).ToListAsync();
+                var result = new List<OrderDto>();
+                foreach (var order in allOrders)
+                {
+                    result.Add(await GetOrderInfo(token, order.Id));
+                }
+                return result;
             }
-            return result;
+            catch (Exception e)
+            {
+                throw new BadHttpRequestException(e.Message);
+            }
         }
 
         public async Task<OrderDto> GetOrderInfo(string token, Guid orderId)
         {
             await ValidateToken(token);
             await ValidateOrder(token, orderId);
-            var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
-            var orderOfAUserEntity = await _context.Orders.Where(x => x.Id == orderId && x.User.Email == tokenInDB.userEmail).FirstOrDefaultAsync();
-            var currentDishBasketDtoList = new List<DishBasketDto>();
-            var dishesInOrder = await _context.DishesInCart.Where(x => x.Order == orderOfAUserEntity).ToListAsync();
-            foreach (var DishInCart in dishesInOrder)
+            try
             {
-                currentDishBasketDtoList.Add(new DishBasketDto()
+                var tokenInDB = await _context.Tokens.Where(x => token == x.Token).FirstOrDefaultAsync();
+                var orderOfAUserEntity = await _context.Orders.Where(x => x.Id == orderId && x.User.Email == tokenInDB.userEmail).FirstOrDefaultAsync();
+                var currentDishBasketDtoList = new List<DishBasketDto>();
+                var dishesInOrder = await _context.DishesInCart.Where(x => x.Order == orderOfAUserEntity).ToListAsync();
+                foreach (var DishInCart in dishesInOrder)
                 {
-                    Id = DishInCart.Id,
-                    Name = DishInCart.Dish.Name,
-                    Price = DishInCart.Price,
-                    TotalPrice = DishInCart.Price * DishInCart.Amount,
-                    Amount = DishInCart.Amount,
-                    Image = DishInCart.Dish.Photo
-                });
+                    currentDishBasketDtoList.Add(new DishBasketDto()
+                    {
+                        Id = DishInCart.Id,
+                        Name = DishInCart.Dish.Name,
+                        Price = DishInCart.Price,
+                        TotalPrice = DishInCart.Price * DishInCart.Amount,
+                        Amount = DishInCart.Amount,
+                        Image = DishInCart.Dish.Photo
+                    });
+                }
+                var result = new OrderDto()
+                {
+                    Id = orderId,
+                    DeliveryTime = orderOfAUserEntity.DeliveryTime,
+                    OrderTime = orderOfAUserEntity.OrderTime,
+                    Status = orderOfAUserEntity.Status,
+                    Price = orderOfAUserEntity.Price,
+                    Dishes = currentDishBasketDtoList,
+                    Address = orderOfAUserEntity.AddresId
+                };
+                return result;
             }
-            var result = new OrderDto()
+            catch (Exception e)
             {
-                Id = orderId,
-                DeliveryTime = orderOfAUserEntity.DeliveryTime,
-                OrderTime = orderOfAUserEntity.OrderTime,
-                Status = orderOfAUserEntity.Status,
-                Price = orderOfAUserEntity.Price,
-                Dishes = currentDishBasketDtoList,
-                Address = orderOfAUserEntity.AddresId
-            };
-            return result;
+                throw new BadHttpRequestException(e.Message);
+            }
         }
 
         private async Task ValidateOrder(string token, Guid orderId)
